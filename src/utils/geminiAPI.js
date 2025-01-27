21af03321207850c0b03d4bd7e17665036c4f993
Balaji Chettiar <147179892+BalajiChettiar@users.noreply.github.com>
2025-01-28 00:03:38 +0530

    Update geminiAPI.js

--- src/utils/geminiAPI.js
+++ src/utils/geminiAPI.js
@@ -1,7 +1,7 @@
 import {GoogleGenerativeAI} from "@google/generative-ai"
-import process from "process";
+import GEMINI_API_KEY from "./constant.js"
 
-const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 export default model 
 
