1991e7501749537de14404639bb5c543436c4800
BalajiChettiar <balajichettiar2004@gmail.com>
2025-01-27 23:32:24 +0530

    added imp credential to .env file

--- src/utils/geminiAPI.js
+++ src/utils/geminiAPI.js
@@ -1,6 +1,7 @@
 import {GoogleGenerativeAI} from "@google/generative-ai"
-import GEMINI_API_KEY from "./constant"
-const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+import process from "process";
+
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 export default model 
 
